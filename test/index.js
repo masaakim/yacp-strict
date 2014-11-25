1686ed192111fbee41c46ac49971f4901f96c98a
Masaaki Morishita <ikasama3.0@gmail.com>
2014-11-25 22:50:02 +0900

    add test code

--- test/index.js
+++ test/index.js
@@ -1,6 +1,31 @@
-var test = require('colored-tape')
-var yacp-strict = require('..')
+var fs = require('fs')
+var postcss = require('postcss')
+var strict = require('..')
 
-test('description', function (t) {
-  t.end()
+var test = require('tape')
+
+function fixture (name) {
+    return fs.readFileSync('test/fixtures/' + name + '.css', 'utf-8').trim()
+}
+
+test('throw error: using not only class selector', function (t) {
+    var res = function () {
+        return postcss().use(strict.postcss).process(fixture('test-1')).css
+    }
+    t.throws(res, /using not only class selector/)
+    t.end()
+})
+
+test('throw error: using !important', function (t) {
+    var res = function () {
+        return postcss().use(strict.postcss).process(fixture('test-2')).css
+    }
+    t.throws(res, /using !important/)
+    t.end()
+})
+
+test('success', function (t) {
+    var res = postcss().use(strict.postcss).process(fixture('test-3')).css
+    t.equal(res, fixture('test-3'))
+    t.end()
 })
